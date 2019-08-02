a912bea718007d50d40d9b0026040eeba0916134
Chase Southard <chase.southard@gmail.com>
2019-08-02 15:44:39 -0400

    replaced dummy with chase

--- src/components/Me.js
+++ src/components/Me.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Me extends React.Component {
   render(){
-    return <h1>Hello, I'm INSERT NAME HERE</h1>;
+    return <h1>Hello, I'm Chase</h1>;
   }
 }
 
