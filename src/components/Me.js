5a533de1492fa30960798e6691f33d3bd4424e9e
Chase Southard <chase.southard@gmail.com>
2019-08-02 16:08:17 -0400

    adds props as the method for displaying name

--- src/components/Me.js
+++ src/components/Me.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Me extends React.Component {
   render(){
-    return <h1>Hello, I'm Chase</h1>;
+    return <h1>Hello, I'm {this.props.name}</h1>;
   }
 }
 
